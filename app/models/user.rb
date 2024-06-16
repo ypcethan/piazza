c6e55e35fac012f7f8d1df2f7f38f922012a95d9
Ethan Chen <ypcethan@gmail.com>
2024-06-16 14:07:52 +1000

    set up user password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :password, length: { minimum: 8 }
   validates :name, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,14 +2,14 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   test "requires a name" do
-    user = User.new(name: "", email: '')
+    user = User.new(name: "", email: 'demo@gmail.com', password: "password")
     assert_not user.valid?
     user.name = 'John'
     assert user.valid?
   end
 
   test "requires a valid email" do
-    user = User.new(name: "John", email: "john")
+    user = User.new(name: "John", email: "john", password: "password")
     assert_not user.valid?
 
     user.email = "john@example.com"
@@ -17,10 +17,10 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "requires a unique email" do
-    existing_user = User.create(name: "John", email: "john@example.com")
+    existing_user = User.create(name: "John", email: "john@example.com", password: "password")
     assert existing_user.persisted?
 
-    another_user = User.create(name: "John", email: "john@example.com")
+    another_user = User.create(name: "John", email: "john@example.com", password: "password")
     assert_not another_user.persisted?
   end
 
@@ -28,9 +28,21 @@ class UserTest < ActiveSupport::TestCase
     user = User.create(
       name: " John  ",
       email: " JOHN@example.com ",
+      password: "password"
     )
 
     assert_equal "John", user.name
     assert_equal "john@example.com", user.email
+  end
+
+  test "password length must be between 8 and ActiveModel's maximum" do
+    user = User.new(name: "John", email: "john@example", password: "")
+    assert_not user.valid?
+
+    user.password = "password"
+    assert user.valid?
+
+    user.password = "a" * (ActiveModel::SecurePassword::MAX_PASSWORD_LENGTH_ALLOWED + 1)
+    assert_not user.valid?
   end
 end
